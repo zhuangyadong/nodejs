82ab3f05d80ae82f3f0ae43ff6f996f19c33818b
zhuangyadong <876573617@qq.com>
2017-06-19 22:54:55 +0800

    .

---
+++ one/fsA.js
@@ -0,0 +1,10 @@
+'use strict';
+var fs = require('fs');
+fs.readFile('timg.jpg',function (err,data) {
+	if(err){
+		console.log(err);
+	}else{
+		console.log(data);
+		console.log(data.length+'bytes');
+	}
+})
