253d224239906a6e8748f02d874ff92438bcd4d0
zhuangyadong <876573617@qq.com>
2017-06-24 17:57:16 +0800

    Update fsB.js

--- one/fsB.js
+++ one/fsB.js
@@ -14,10 +14,8 @@ fs.stat('sample.txt',function (err,stat) {
 		if(stat.isFile()){
 			//文件大小
 			console.log('size:'+stat.size);
-
 			//创建时间， date对象
 			console.log('birth time:'+stat.birthtime);
-
 			//修改时间 Date对象
 			console.log('modified time:'+stat.mtime);
 		}
